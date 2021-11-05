401b4fb83ca784d5655cbc2d4384edf545a24ead
Jeremy Huynh <jeahuynh18@gmail.com>
2021-11-04 21:02:37 -0700

    cleaned up script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,3 @@
-//   document.querySelector("#form").addEventListener("submit", (event) => {
-//     event.preventDefault();
 document
   .querySelector("#COVID-search-form")
   .addEventListener("submit", (event) => {
@@ -10,44 +8,18 @@ document
       document.getElementById("state").value =
     state.options[state.selectedIndex].text;
     }
-    // console.log("*****",state);
     
-
-    // const state = 
-    // console.log("State Selector: ", document.getElementById("state"))
     var stateCode = document.getElementById("state").value;
     fetch(
       `https://api.covidactnow.org/v2/state/${stateCode}.json?apiKey=bf076c55a54247e998486f5cc1ba1acc`,
-      // {
-      //   method: "GET",
-      //   headers: {
-      //     "host": "api.covidactnow.org",
-      //     "api-key": "bf076c55a54247e998486f5cc1ba1acc"
-      //   }
-      // }
-      //   //`https://covid-19-statistics.p.rapidapi.com/reports?city_name=${cityName}&${stateName}`,
-      //   //{
-      //   //  method: "GET",
-      //   //  headers: {
-      //       "x-rapidapi-host": "covid-19-statistics.p.rapidapi.com",
-      //       "x-rapidapi-key":
-      //         "50bfd5e59dmsh8c894a8811452bep172e1ejsnbaab7a0b6b6b",
-      //     },
-      //   }
     )
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
         console.log("*****", data.actuals);
         console.log("jajajja", data.actuals.cases);
-        // var i = data.actuals[0]
-        // for (i in data.actuals.length){
-        //   console.log("hehe",i);
-        // }
+        
         let template = "";
-          // data.actuals.forEach((datum) => {
-          //   console.log("!!!!!",datum)
-          // });
           template += `
             <div>
               <h3>State: ${stateCode}</h3>
@@ -81,34 +53,5 @@ document
       .catch((err) => {
         console.error(err);
       })
-    //    // collect form data
+    // collect form data
   });
-
-
-
-  /// add back to template later
-
-  // <p>State: ${datum.province}</p>
-  //<p>Recovered: ${datum.recovered}</p>
-  // <h3>City</h3>
-  // <p>City Name: ${datum.region.cities[0].name}</p>
-  // <p>Death: ${datum.region.cities[0].deaths}</p>
-  // <p>Recovered: ${datum.region.cities[0].recovered}</p>
-
-
-
-
-
-// const state = document.querySelector("#state").value;
-
-// console.log(state, latitude, longitude);
-// state = province
-//latitude = lat
-// longitude = long
-//cities {name:""}
-
-// variables for each state value equal to the state name
-// user inputs their lat long
-// compare to the lat and long of the nearest county
-// show in main
-//   });;

0a166acfb1f1cc7fbb4fc84a54ace422d96b52a4
Kaylin Boyle <kaylinnboyle@gmail.com>
2021-11-06 21:23:20 -0700

    local storage aspect added

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,5 @@
+// document.
+
 document
   .querySelector("#COVID-search-form")
   .addEventListener("submit", (event) => {
@@ -16,9 +18,23 @@ document
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        console.log("*****", data.actuals);
-        console.log("stateLatLng_Coordinates", stateLocationMapping);
+        
 
+        var states = JSON.parse(localStorage.getItem("states")) || []
+        var filteredStates = states.filter(function(state){
+          return state.code === stateCode;
+        })
+        if(filteredStates.length <= 0) {
+          var stateData = {
+            code: stateCode,
+            newCases: data.actuals.newCases,
+            cases: data.actuals.cases,
+            deaths: data.actuals.deaths
+          }
+          states.push(stateData)
+          localStorage.setItem("states", JSON.stringify(states))
+        };
+        
         var currLat
         var currLng
         for (i = 0; i < stateLocationMapping.length; i++) {
@@ -63,12 +79,63 @@ document
             
           
         // });
-        console.log(template);
-        document.querySelector("#data-insert").innerHTML = template;
-        console.log(document.querySelector("#data-insert"))
+      let temp2 = ""; 
+      var di2 = document.createElement("div");
+      template += temp2;
+      di2.innerHTML = template; 
+      // console.log(temp2); 
+        
+      // });
+      // console.log(template);
+      document.querySelector("#data-insert").appendChild(di2);
+
       })
       .catch((err) => {
         console.error(err);
       })
     // collect form data
   });
+  var statesLocalStorage = JSON.parse(localStorage.getItem("states"));
+  // console.log(statesLocalStorage);
+  if (statesLocalStorage.length>0) {
+    // do for loops for state in the storage
+    // localStorage.forEach(i => console.log(i));
+    for (var i=0; i< statesLocalStorage.length; i++){
+      var s  = statesLocalStorage[i]
+      console.log(s);
+    };
+    // var s  = statesLocalStorage[i];
+  
+    // console.log("yea");
+    
+      let template= "";
+      let temp2 = "";
+      template += `
+            <div>
+              <p> Home State <p>
+              <h3>State: ${s.code}</h3>
+              <p>New Cases: ${s.newCases}</p>
+              <p>Cases: ${s.cases}</p>
+              <p>Deaths: ${s.deaths}</p>
+              <p>Vaccines Administered: 1</p>
+              <p>Negative Tests: 1</p>
+              <p>ICU Beds Capacity: 1</p>
+              <p>ICU Beds Current Total in Use: 1</p>
+            </div>
+              `;
+          temp2 += `
+              <div>
+                <p></p>
+              </div>
+                `;   
+        var di2 = document.createElement("div");
+        template += temp2;
+        di2.innerHTML = template; 
+        console.log(di2); 
+          
+        // });
+        console.log(template);
+        document.querySelector("#data-insert").appendChild(di2);
+        console.log(di2);
+      
+  };
